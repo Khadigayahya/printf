2f036902ed749fc3ec916bd2204af2d607c42bf3
Magda <magdaelsayed03@gmail.com>
2023-06-26 03:13:35 +0300

    Updated

--- number.c
+++ number.c
@@ -49,7 +49,7 @@ int print_unsigned(va_list ap, params_t *params)
 	unsigned long l;
 
 	if (params->l_modifier)
-		l = (unsigned_long)va_arg(ap, unsigned long);
+		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
